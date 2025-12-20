d55b1eb0ac818af351c413cc770e095993881ebd
Aleksandr Salnikov <taynbiy@gmail.com>
2025-12-20 20:09:33 +0500

    fix autofocus

--- src/components/ui/Dialog.tsx
+++ src/components/ui/Dialog.tsx
@@ -50,9 +50,12 @@ function DialogContent({
   className,
   children,
   showCloseButton = true,
+  disableAutoFocus = true,
+  onOpenAutoFocus,
   ...props
 }: React.ComponentProps<typeof DialogPrimitive.Content> & {
   showCloseButton?: boolean
+  disableAutoFocus?: boolean
 }) {
   return (
     <DialogPortal data-slot="dialog-portal">
@@ -63,6 +66,14 @@ function DialogContent({
           "bg-background data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 fixed top-[50%] left-[50%] z-50 grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] gap-4 rounded-3xl border p-6 shadow-lg duration-200 max-h-[calc(100svh-2rem)] overflow-y-auto md:max-h-[70dvh] xl:max-w-5xl",
           className
         )}
+        onOpenAutoFocus={(event) => {
+          if (onOpenAutoFocus) {
+            onOpenAutoFocus(event)
+          }
+          if (!event.defaultPrevented && disableAutoFocus) {
+            event.preventDefault()
+          }
+        }}
         {...props}
       >
         {children}
